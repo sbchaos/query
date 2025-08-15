86e4cc26fda49f1c4cd90f17e54b1be1ea67506b
Sandeep Bhardwaj <7288570+sbchaos@users.noreply.github.com>
2025-08-15 14:34:35 +0530

    add scanner

---
+++ scanner.go
@@ -0,0 +1,411 @@
+package query
+
+import (
+	"bytes"
+	"io"
+	"unicode"
+)
+
+type Scanner struct {
+	r   io.RuneReader
+	buf bytes.Buffer
+
+	ch   rune
+	pos  Pos
+	full bool
+}
+
+func NewScanner(r io.RuneReader) *Scanner {
+	return &Scanner{
+		r:   r,
+		pos: Pos{Offset: -1, Line: 1},
+	}
+}
+
+func (s *Scanner) Scan() (pos Pos, token Token, lit string) {
+	for {
+		if ch := s.peek(); ch == -1 {
+			return s.pos, EOF, ""
+		} else if unicode.IsSpace(ch) {
+			s.read()
+			continue
+		} else if isDigit(ch) || ch == '.' {
+			return s.scanNumber()
+		} else if ch == 'x' || ch == 'X' {
+			return s.scanBlob()
+		} else if isAlpha(ch) || ch == '_' {
+			return s.scanUnquotedIdent(s.pos, "")
+		} else if ch == '"' {
+			return s.scanQuotedIdent()
+		} else if ch == '\'' {
+			return s.scanString()
+		} else if ch == '?' || ch == ':' || ch == '@' || ch == '$' {
+			return s.scanBind()
+		}
+
+		switch ch, pos := s.read(); ch {
+		case ';':
+			return pos, SEMI, ";"
+		case '(':
+			return pos, LP, "("
+		case ')':
+			return pos, RP, ")"
+		case ',':
+			return pos, COMMA, ","
+		case '!':
+			if s.peek() == '=' {
+				s.read()
+				return pos, NE, "!="
+			}
+			return pos, ILLEGAL, "!"
+		case '=':
+			if s.peek() == '=' {
+				s.read()
+				return pos, EQ, "=="
+			}
+			return pos, EQ, "="
+		case '<':
+			if s.peek() == '=' {
+				s.read()
+				return pos, LE, "<="
+			} else if s.peek() == '<' {
+				s.read()
+				return pos, LSHIFT, "<<"
+			} else if s.peek() == '>' {
+				s.read()
+				return pos, NE, "<>"
+			}
+			return pos, LT, "<"
+		case '>':
+			if s.peek() == '=' {
+				s.read()
+				return pos, GE, ">="
+			} else if s.peek() == '>' {
+				s.read()
+				return pos, RSHIFT, ">>"
+			}
+			return pos, GT, ">"
+		case '&':
+			return pos, BITAND, "&"
+		case '|':
+			if s.peek() == '|' {
+				s.read()
+				return pos, CONCAT, "||"
+			}
+			return pos, BITOR, "|"
+		case '+':
+			return pos, PLUS, "+"
+		case '-':
+			if s.peek() == '>' {
+				s.read()
+				if s.peek() == '>' {
+					s.read()
+					return pos, JSON_EXTRACT_SQL, "->>"
+				}
+				return pos, JSON_EXTRACT_JSON, "->"
+			} else if s.peek() == '-' {
+				s.read()
+				return pos, COMMENT, s.scanSingleLineComment()
+			}
+			return pos, MINUS, "-"
+		case '*':
+			return pos, STAR, "*"
+		case '/':
+			if s.peek() == '*' {
+				s.read()
+				return pos, COMMENT, s.scanMultiLineComment()
+			}
+			return pos, SLASH, "/"
+		case '%':
+			return pos, REM, "%"
+		case '~':
+			return pos, BITNOT, "~"
+		default:
+			return pos, ILLEGAL, string(ch)
+		}
+	}
+}
+
+func (s *Scanner) scanUnquotedIdent(pos Pos, prefix string) (Pos, Token, string) {
+	assert(isUnquotedIdent(s.peek()))
+
+	s.buf.Reset()
+	s.buf.WriteString(prefix)
+	for ch, _ := s.read(); isUnquotedIdent(ch); ch, _ = s.read() {
+		s.buf.WriteRune(ch)
+	}
+	s.unread()
+
+	lit := s.buf.String()
+	tok := Lookup(lit)
+	return pos, tok, lit
+}
+
+func (s *Scanner) scanQuotedIdent() (Pos, Token, string) {
+	ch, pos := s.read()
+	assert(ch == '"')
+
+	s.buf.Reset()
+	for {
+		ch, _ := s.read()
+		if ch == -1 {
+			return pos, ILLEGAL, `"` + s.buf.String()
+		} else if ch == '"' {
+			if s.peek() == '"' { // escaped quote
+				s.read()
+				s.buf.WriteRune('"')
+				continue
+			}
+			return pos, QIDENT, s.buf.String()
+		}
+		s.buf.WriteRune(ch)
+	}
+}
+
+func (s *Scanner) scanString() (Pos, Token, string) {
+	ch, pos := s.read()
+	assert(ch == '\'')
+
+	s.buf.Reset()
+	for {
+		ch, _ := s.read()
+		if ch == -1 {
+			return pos, ILLEGAL, `'` + s.buf.String()
+		} else if ch == '\'' {
+			if s.peek() == '\'' { // escaped quote
+				s.read()
+				s.buf.WriteRune('\'')
+				continue
+			}
+			return pos, STRING, s.buf.String()
+		}
+		s.buf.WriteRune(ch)
+	}
+}
+
+func (s *Scanner) scanSingleLineComment() string {
+	s.buf.Reset()
+	s.buf.WriteString("--")
+
+	for {
+		ch, _ := s.read()
+		switch ch {
+		case -1, '\n':
+			return s.buf.String()
+		default:
+			s.buf.WriteRune(ch)
+		}
+	}
+}
+
+func (s *Scanner) scanMultiLineComment() string {
+	s.buf.Reset()
+	s.buf.WriteString("/*")
+	for {
+		ch, _ := s.read()
+		if ch == -1 {
+			return s.buf.String()
+		} else if ch == '*' && s.peek() == '/' {
+			s.read()
+			s.buf.WriteString("*/")
+			return s.buf.String()
+		}
+		s.buf.WriteRune(ch)
+	}
+}
+
+func (s *Scanner) scanBind() (Pos, Token, string) {
+	start, pos := s.read()
+
+	s.buf.Reset()
+	s.buf.WriteRune(start)
+
+	// Question mark starts a numeric bind.
+	if start == '?' {
+		for isDigit(s.peek()) {
+			ch, _ := s.read()
+			s.buf.WriteRune(ch)
+		}
+		return pos, BIND, s.buf.String()
+	}
+
+	// All other characters start an alphanumeric bind.
+	assert(start == ':' || start == '@' || start == '$')
+	for isUnquotedIdent(s.peek()) {
+		ch, _ := s.read()
+		s.buf.WriteRune(ch)
+	}
+	return pos, BIND, s.buf.String()
+}
+
+func (s *Scanner) scanBlob() (Pos, Token, string) {
+	start, pos := s.read()
+	assert(start == 'x' || start == 'X')
+
+	// If the next character is not a quote, it's an IDENT.
+	if isUnquotedIdent(s.peek()) {
+		return s.scanUnquotedIdent(pos, string(start))
+	} else if s.peek() != '\'' {
+		return pos, IDENT, string(start)
+	}
+	ch, _ := s.read()
+	assert(ch == '\'')
+
+	s.buf.Reset()
+	for i := 0; ; i++ {
+		ch, _ := s.read()
+		if ch == '\'' {
+			return pos, BLOB, s.buf.String()
+		} else if ch == -1 {
+			return pos, ILLEGAL, string(start) + `'` + s.buf.String()
+		} else if !isHex(ch) {
+			return pos, ILLEGAL, string(start) + `'` + s.buf.String() + string(ch)
+		}
+		s.buf.WriteRune(ch)
+	}
+}
+
+func (s *Scanner) scanNumber() (Pos, Token, string) {
+	assert(isDigit(s.peek()) || s.peek() == '.')
+	pos := s.pos
+	tok := INTEGER
+
+	s.buf.Reset()
+
+	if s.peek() == '0' {
+		s.buf.WriteRune('0')
+		s.read()
+		if s.peek() == 'x' || s.peek() == 'X' {
+			s.read()
+			s.buf.WriteRune('x')
+			for isHex(s.peek()) {
+				ch, _ := s.read()
+				s.buf.WriteRune(ch)
+			}
+			// TODO: error handling:
+			// if len(s.buf.String()) < 2 => invalid
+			// reason: means we scanned '0x'
+			// if len(s.buf.String()) - 2 > 16 => invalid
+			// reason: according to spec maximum of 16 significant digits)
+			return pos, tok, s.buf.String()
+		}
+	}
+
+	// Read whole number if starting with a digit.
+	if isDigit(s.peek()) {
+		for isDigit(s.peek()) {
+			ch, _ := s.read()
+			s.buf.WriteRune(ch)
+		}
+	}
+
+	// Read decimal and successive digits.
+	if s.peek() == '.' {
+		tok = FLOAT
+
+		ch, _ := s.read()
+		s.buf.WriteRune(ch)
+
+		for isDigit(s.peek()) {
+			ch, _ := s.read()
+			s.buf.WriteRune(ch)
+		}
+	}
+
+	// If we just have a dot in the buffer with no digits by this point,
+	// this can't be a number, so we can stop and return DOT
+	if s.buf.String() == "." {
+		return pos, DOT, "."
+	}
+
+	// Read exponent with optional +/- sign.
+	if ch := s.peek(); ch == 'e' || ch == 'E' {
+		tok = FLOAT
+
+		ch, _ := s.read()
+		s.buf.WriteRune(ch)
+
+		if s.peek() == '+' || s.peek() == '-' {
+			ch, _ := s.read()
+			s.buf.WriteRune(ch)
+			if !isDigit(s.peek()) {
+				return pos, ILLEGAL, s.buf.String()
+			}
+			for isDigit(s.peek()) {
+				ch, _ := s.read()
+				s.buf.WriteRune(ch)
+			}
+		} else if isDigit(s.peek()) {
+			for isDigit(s.peek()) {
+				ch, _ := s.read()
+				s.buf.WriteRune(ch)
+			}
+		} else {
+			return pos, ILLEGAL, s.buf.String()
+		}
+	}
+
+	return pos, tok, s.buf.String()
+}
+
+func (s *Scanner) read() (rune, Pos) {
+	if s.full {
+		s.full = false
+		return s.ch, s.pos
+	}
+
+	var err error
+	s.ch, _, err = s.r.ReadRune()
+	if err != nil {
+		s.ch = -1
+		return s.ch, s.pos
+	}
+
+	s.pos.Offset++
+	if s.ch == '\n' {
+		s.pos.Line++
+		s.pos.Column = 0
+	} else {
+		s.pos.Column++
+	}
+	return s.ch, s.pos
+}
+
+func (s *Scanner) peek() rune {
+	if !s.full {
+		s.read()
+		s.unread()
+	}
+	return s.ch
+}
+
+func (s *Scanner) unread() {
+	assert(!s.full)
+	s.full = true
+}
+
+func isDigit(ch rune) bool {
+	return ch >= '0' && ch <= '9'
+}
+
+func isAlpha(ch rune) bool {
+	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
+}
+
+func isHex(ch rune) bool {
+	return isDigit(ch) || (ch >= 'a' && ch <= 'f') || (ch >= 'A' && ch <= 'F')
+}
+
+func isUnquotedIdent(ch rune) bool {
+	return isAlpha(ch) || isDigit(ch) || ch == '_'
+}
+
+// IsInteger returns true if s only contains digits.
+func IsInteger(s string) bool {
+	for _, ch := range s {
+		if !isDigit(ch) {
+			return false
+		}
+	}
+	return s != ""
+}
---
+++ scanner_test.go
@@ -0,0 +1,221 @@
+package query_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/sbchaos/query"
+)
+
+func TestScanner_Scan(t *testing.T) {
+	t.Run("IDENT", func(t *testing.T) {
+		t.Run("Unquoted", func(t *testing.T) {
+			AssertScan(t, `foo_BAR123`, query.IDENT, `foo_BAR123`)
+		})
+		t.Run("Quoted", func(t *testing.T) {
+			AssertScan(t, `"crazy ~!#*&# column name"" foo"`, query.QIDENT, `crazy ~!#*&# column name" foo`)
+		})
+		t.Run("NoEndQuote", func(t *testing.T) {
+			AssertScan(t, `"unfinished`, query.ILLEGAL, `"unfinished`)
+		})
+		t.Run("x", func(t *testing.T) {
+			AssertScan(t, `x`, query.IDENT, `x`)
+		})
+		t.Run("StartingX", func(t *testing.T) {
+			AssertScan(t, `xyz`, query.IDENT, `xyz`)
+		})
+	})
+
+	t.Run("COMMENT", func(t *testing.T) {
+		t.Run("SingleLine", func(t *testing.T) {
+			t.Run("Newline", func(t *testing.T) {
+				AssertScan(t, "-- foo bar\n--baz", query.COMMENT, `-- foo bar`)
+			})
+			t.Run("EOF", func(t *testing.T) {
+				AssertScan(t, "-- foo bar", query.COMMENT, `-- foo bar`)
+			})
+			t.Run("NoContent", func(t *testing.T) {
+				AssertScan(t, "--", query.COMMENT, `--`)
+			})
+		})
+		t.Run("MultiLine", func(t *testing.T) {
+			t.Run("Newline", func(t *testing.T) {
+				AssertScan(t, "/* foo bar */", query.COMMENT, `/* foo bar */`)
+			})
+			t.Run("EOF", func(t *testing.T) {
+				AssertScan(t, "/* foo bar", query.COMMENT, `/* foo bar`)
+			})
+			t.Run("NoContent", func(t *testing.T) {
+				AssertScan(t, "/**/", query.COMMENT, `/**/`)
+			})
+		})
+	})
+
+	t.Run("KEYWORD", func(t *testing.T) {
+		AssertScan(t, `BEGIN`, query.BEGIN, `BEGIN`)
+	})
+
+	t.Run("STRING", func(t *testing.T) {
+		t.Run("OK", func(t *testing.T) {
+			AssertScan(t, `'this is ''a'' string'`, query.STRING, `this is 'a' string`)
+		})
+		t.Run("NoEndQuote", func(t *testing.T) {
+			AssertScan(t, `'unfinished`, query.ILLEGAL, `'unfinished`)
+		})
+	})
+	t.Run("BLOB", func(t *testing.T) {
+		t.Run("LowerX", func(t *testing.T) {
+			AssertScan(t, `x'0123456789abcdef'`, query.BLOB, `0123456789abcdef`)
+		})
+		t.Run("UpperX", func(t *testing.T) {
+			AssertScan(t, `X'0123456789ABCDEF'`, query.BLOB, `0123456789ABCDEF`)
+		})
+		t.Run("NoEndQuote", func(t *testing.T) {
+			AssertScan(t, `x'0123`, query.ILLEGAL, `x'0123`)
+		})
+		t.Run("BadHex", func(t *testing.T) {
+			AssertScan(t, `x'hello`, query.ILLEGAL, `x'h`)
+		})
+	})
+
+	t.Run("INTEGER", func(t *testing.T) {
+		AssertScan(t, `012`, query.INTEGER, `012`)
+		AssertScan(t, `123`, query.INTEGER, `123`)
+		AssertScan(t, `0xe3`, query.INTEGER, `0xe3`)
+		// BUG: see comment in scanner
+		// AssertScanError(t, `0x`, query.ILLEGAL)
+		// AssertScanError(t, `4xe3`, query.ILLEGAL)
+		// AssertScanError(t, `0x12345678912345678`, query.ILLEGAL, ``)
+	})
+
+	t.Run("FLOAT", func(t *testing.T) {
+		AssertScan(t, `123.456`, query.FLOAT, `123.456`)
+		AssertScan(t, `0.01`, query.FLOAT, `0.01`)
+		AssertScan(t, `.1`, query.FLOAT, `.1`)
+		AssertScan(t, `123e456`, query.FLOAT, `123e456`)
+		AssertScan(t, `123E456`, query.FLOAT, `123E456`)
+		AssertScan(t, `123.456E78`, query.FLOAT, `123.456E78`)
+		AssertScan(t, `123.E45`, query.FLOAT, `123.E45`)
+		AssertScan(t, `123E+4`, query.FLOAT, `123E+4`)
+		AssertScan(t, `123E-4`, query.FLOAT, `123E-4`)
+		AssertScan(t, `.0E-2`, query.FLOAT, `.0E-2`)
+		AssertScan(t, `123E`, query.ILLEGAL, `123E`)
+		AssertScan(t, `123E+`, query.ILLEGAL, `123E+`)
+		AssertScan(t, `123E-`, query.ILLEGAL, `123E-`)
+	})
+	t.Run("BIND", func(t *testing.T) {
+		AssertScan(t, `?'`, query.BIND, `?`)
+		AssertScan(t, `?123'`, query.BIND, `?123`)
+		AssertScan(t, `:foo_bar123'`, query.BIND, `:foo_bar123`)
+		AssertScan(t, `@bar'`, query.BIND, `@bar`)
+		AssertScan(t, `$baz'`, query.BIND, `$baz`)
+	})
+
+	t.Run("EOF", func(t *testing.T) {
+		AssertScan(t, " \n\t\r", query.EOF, ``)
+	})
+
+	t.Run("SEMI", func(t *testing.T) {
+		AssertScan(t, ";", query.SEMI, ";")
+	})
+	t.Run("LP", func(t *testing.T) {
+		AssertScan(t, "(", query.LP, "(")
+	})
+	t.Run("RP", func(t *testing.T) {
+		AssertScan(t, ")", query.RP, ")")
+	})
+	t.Run("COMMA", func(t *testing.T) {
+		AssertScan(t, ",", query.COMMA, ",")
+	})
+	t.Run("NE", func(t *testing.T) {
+		AssertScan(t, "!=", query.NE, "!=")
+		AssertScan(t, "<>", query.NE, "<>")
+	})
+	t.Run("BITNOT", func(t *testing.T) {
+		AssertScan(t, "~", query.BITNOT, "~")
+	})
+	t.Run("EQ", func(t *testing.T) {
+		AssertScan(t, "=", query.EQ, "=")
+		AssertScan(t, "==", query.EQ, "==")
+	})
+	t.Run("LE", func(t *testing.T) {
+		AssertScan(t, "<=", query.LE, "<=")
+	})
+	t.Run("LSHIFT", func(t *testing.T) {
+		AssertScan(t, "<<", query.LSHIFT, "<<")
+	})
+	t.Run("LT", func(t *testing.T) {
+		AssertScan(t, "<", query.LT, "<")
+	})
+	t.Run("GE", func(t *testing.T) {
+		AssertScan(t, ">=", query.GE, ">=")
+	})
+	t.Run("RSHIFT", func(t *testing.T) {
+		AssertScan(t, ">>", query.RSHIFT, ">>")
+	})
+	t.Run("GT", func(t *testing.T) {
+		AssertScan(t, ">", query.GT, ">")
+	})
+	t.Run("BITAND", func(t *testing.T) {
+		AssertScan(t, "&", query.BITAND, "&")
+	})
+	t.Run("CONCAT", func(t *testing.T) {
+		AssertScan(t, "||", query.CONCAT, "||")
+	})
+	t.Run("BITOR", func(t *testing.T) {
+		AssertScan(t, "|", query.BITOR, "|")
+	})
+	t.Run("PLUS", func(t *testing.T) {
+		AssertScan(t, "+", query.PLUS, "+")
+	})
+	t.Run("MINUS", func(t *testing.T) {
+		AssertScan(t, "-", query.MINUS, "-")
+	})
+	t.Run("STAR", func(t *testing.T) {
+		AssertScan(t, "*", query.STAR, "*")
+	})
+	t.Run("SLASH", func(t *testing.T) {
+		AssertScan(t, "/", query.SLASH, "/")
+	})
+	t.Run("REM", func(t *testing.T) {
+		AssertScan(t, "%", query.REM, "%")
+	})
+	t.Run("DOT", func(t *testing.T) {
+		AssertScan(t, ".", query.DOT, ".")
+		AssertScan(t, `.E2`, query.DOT, `.`)
+	})
+	t.Run("JSON_EXTRACT_JSON", func(t *testing.T) {
+		AssertScan(t, "->", query.JSON_EXTRACT_JSON, "->")
+	})
+	t.Run("JSON_EXTRACT_SQL", func(t *testing.T) {
+		AssertScan(t, "->>", query.JSON_EXTRACT_SQL, "->>")
+	})
+	t.Run("ILLEGAL", func(t *testing.T) {
+		AssertScan(t, "^", query.ILLEGAL, "^")
+	})
+}
+
+// AssertScan asserts the value of the first scan to s.
+func AssertScan(tb testing.TB, s string, expectedTok query.Token, expectedLit string) {
+	tb.Helper()
+	_, tok, lit := query.NewScanner(strings.NewReader(s)).Scan()
+	if tok != expectedTok || lit != expectedLit {
+		tb.Fatalf("Scan(%q)=<%s,%s>, want <%s,%s>", s, tok, lit, expectedTok, expectedLit)
+	}
+}
+
+func Benchmark_NewScanner(b *testing.B) {
+	s := `SELECT * FROM foo WHERE bar = 1`
+	for i := 0; i < b.N; i++ {
+		scanner := query.NewScanner(strings.NewReader(s))
+		for {
+			_, tok, lit := scanner.Scan()
+			if tok == query.EOF {
+				break
+			}
+			if tok == query.ILLEGAL {
+				b.Fatalf("Unexpected ILLEGAL token: %s", lit)
+			}
+		}
+	}
+}
